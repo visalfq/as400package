d2e93fcfd2a39885f0ffaaa5109bca094ff8f214
Vishal Jain <vishal.jain@victormedical.com>
2025-02-07 14:24:57 -0800

    changed hellp function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function getstock(item,qty) {
     return "getstock" +";"+ item+";" + qty;
   }
-       function hello(item) {
-        return "Hello" + item;
+       function hellon(item) {
+        return "Hellon" + item;
     }
     function getcustomerinfo(customernumber){
         return "getcustomerino" + customernumber;
     }
  
 exports.getstock = getstock;
-exports.hello = hello;
+exports.hellon = hellon;
 exports.getcustomerinfo = getcustomerinfo;
